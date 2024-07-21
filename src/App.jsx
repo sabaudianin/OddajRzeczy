bc2547d7204beb1ce36741b4e065ecb113dcb0e8
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 <sabaudianin@gmail.com>
2024-07-21 17:38:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {} from 'react'
+
+
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <div className="App">
+                <h1 className="text-3xl font-bold underline">
+                    Hello, Tailwind with React!
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default App
